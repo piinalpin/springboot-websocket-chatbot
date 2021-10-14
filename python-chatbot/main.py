210cadddf607a19ac36072e4a5085815f74c68a4
Alvinditya Saputra <alvindityas@gmail.com>
2021-10-14 10:14:42 +0700

    add contextual chatbot with python using tensorflow and tf learn

---
+++ python-chatbot/main.py
@@ -0,0 +1,32 @@
+from tensor_flow import TensorFlow
+from flask import Flask, request, jsonify
+
+resp = TensorFlow(intents="intents.json")
+
+# tag, data = resp.response("halo")
+# print(data)
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def chat():
+    tag, data = resp.response(request.json['message'])
+    response_dto = dict()
+    response_dto["type"] = tag
+    if tag == "riddles":
+        response_data = dict()
+        response_data["question"] = data["question"]
+        response_data["answer"] = data["answer"]
+        response_dto["data"] = response_data
+    else:
+        response_data = dict()
+        response_data["message"] = data
+        response_dto["data"] = response_data
+
+    response = jsonify(response_dto)
+    response.status_code = 200
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000, debug=True)
